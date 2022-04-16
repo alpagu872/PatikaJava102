19f0ff5ae563611d32c233119e133c0e25fb410c
Alpagu E <83309872+alpagu872@users.noreply.github.com>
2022-04-16 11:53:16 +0300

    11:53 AM

---
+++ src/Package/Main.java
@@ -0,0 +1,8 @@
+package Package;
+
+public class Main {
+    public static void main(String[] args) {
+        AccessModifiers a = new AccessModifiers("Selam");
+        a.show();
+    }
+}
---
+++ src/Second/Second.java
@@ -0,0 +1,10 @@
+package Second;
+
+import Package.*;
+
+public class Second {
+    public static void main(String[] args) {
+       AccessModifiers ab = new AccessModifiers("SELAM sec");
+     //   ab.show();
+    }
+}

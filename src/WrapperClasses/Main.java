49fde8af4b033f024579a6d9a96f575648776f60
Alpagu E <83309872+alpagu872@users.noreply.github.com>
2022-04-26 00:08:39 +0300

    Boxing & Unboxing || 12:08 AM

---
+++ src/WrapperClasses/Main.java
@@ -0,0 +1,19 @@
+package WrapperClasses;
+
+public class Main {
+
+
+    public static void main(String[] args) {
+
+        String a = "10";
+        int b = 20;
+        int c = Integer.parseInt(a); //string to int
+
+
+        System.out.println(Integer.parseInt(a) + b);
+
+
+    }
+
+
+}

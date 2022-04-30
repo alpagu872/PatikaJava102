8cf42c72e199ae5d8109a02e3991ba2d3be12676
Alpagu E <alpagu872@gmail.com>
2022-04-30 03:49:58 +0300

    Exception Handling || 3:49 AM

--- src/ExceptionHandling/ThrowUsing/Main.java
+++ src/ExceptionHandling/ThrowUsing/Main.java
@@ -3,18 +3,22 @@ package ExceptionHandling.ThrowUsing;
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args){
         Scanner girdi = new Scanner(System.in);
         int age = girdi.nextInt();
-        checkAge(age);
+        try {
+            checkAge(age);
+        }catch (Exception e){
+            System.out.println("Hata");
+        }
 
         System.out.println("Program bitti.");
 
     }
 
-    public static void checkAge(int age) {
+    public static void checkAge(int age) throws Exception {
         if (age < 18) {
-            throw new ArithmeticException("Dışarı çıkamazsınız");
+            throw new Exception("Dışarı çıkamazsınız");
         } else {
             System.out.println("Yaşı uygundur.");
         }

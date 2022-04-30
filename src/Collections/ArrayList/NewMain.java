04cd9a581c07b06d207958ecf30da63eb22b831e
Alpagu E <alpagu872@gmail.com>
2022-04-30 10:39:54 +0300

    ArrayList Update || 10:39 AM

---
+++ src/Collections/ArrayList/NewMain.java
@@ -0,0 +1,28 @@
+package Collections.ArrayList;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class NewMain {
+    public static void main(String[] args) {
+        List<String> liste = new ArrayList<>();
+        liste.add("Ankara");
+        liste.add("Urfa");
+        liste.add("İzmir");
+        liste.add("Çanakkale");
+        liste.add("İstanbul");
+        System.out.println(liste);
+
+        liste.remove(0);
+        System.out.println(liste);
+
+        List<String> liste2 = new ArrayList<>();
+        liste2.add("Erzurum");
+
+        liste2.addAll(liste);
+
+        System.out.println(liste2);
+
+    }
+}

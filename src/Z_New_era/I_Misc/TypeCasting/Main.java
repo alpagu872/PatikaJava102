0ff1e3b585d0067c7099431c937069c91fcd0b5e
Alpagu E <83309872+alpagu872@users.noreply.github.com>
2022-06-07 13:45:29 +0300

    Misc. || 1:45 PM

---
+++ src/Z_New_era/I_Misc/TypeCasting/Main.java
@@ -0,0 +1,18 @@
+package Z_New_era.I_Misc.TypeCasting;
+
+public class Main {
+    public static void main(String[] args) {
+        int a = 10;
+        double b = a;
+        System.out.println(b);
+
+
+
+        double pi = 3.14;
+        int aa = (int) pi;
+        System.out.println(aa);
+
+        String str = String.valueOf(aa);
+        System.out.println(str);
+    }
+}

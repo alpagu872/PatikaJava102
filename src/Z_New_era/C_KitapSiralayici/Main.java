e3c32f64f5d3c26a52cd917d0f201f5b2e1e68da
Alpagu E <83309872+alpagu872@users.noreply.github.com>
2022-07-18 14:14:27 +0300

    Update book compare

--- src/Z_New_era/C_KitapSiralayici/Main.java
+++ src/Z_New_era/C_KitapSiralayici/Main.java
@@ -1,53 +1,71 @@
 package Z_New_era.C_KitapSiralayici;
 
+import java.util.ArrayList;
 import java.util.Comparator;
-import java.util.TreeSet;
 
 public class Main {
     public static void main(String[] args) {
 
+        // StreamAPI'ler functional programingden sonra ortaya çıkan işimizi baya kolaylaştıran yapılar
 
-        TreeSet<Book> books = new TreeSet<Book>();
+        ArrayList<Integer> list = new ArrayList<>();
+        list.add(15);
+        list.add(23);
+        list.add(45);
+        list.add(17);
+        list.add(90);
+        list.add(90);
+        list.add(90);
 
-        Book book1 = new Book("Küçük Prens", 302, "Antoine de Saint-Exupéry", "2014");
-        Book book2 = new Book("Simyacı", 350, "Paulo coelho", "2014");
-        Book book3 = new Book("Yüzyıllık yalnızlık", 105, "Gabriel Marquez", "2014");
-        Book book4 = new Book("Taht Oyunları", 850, "George Martin", "2014");
-        Book book5 = new Book("Hayvan Çiftliği", 120, "George Orwell", "2014");
+        System.out.println("ForEach");
+        //ForEach
+        list.stream().forEach(i->System.out.println(i));
 
-        books.add(book1);
-        books.add(book2);
-        books.add(book3);
-        books.add(book4);
-        books.add(book5);
+        System.out.println("Filter");
+        //Filter (60 dan büyük olanları yazdır.)
+        list.stream().filter(num -> num >60).forEach(num ->System.out.println(num));
 
-        System.out.println("Ada göre: ");
-        for (Book book : books) {
+        System.out.println("Distance");
+        //Distance (tekrar eden sayıları yazmaz.)
+        list.stream().distinct().forEach(i->System.out.println(i));
 
-            System.out.println("Kitap Adı : " + book.getName() +
-                    ",\t Sayfa Sayısı : " + book.getPageCount() +
-                    ",\t Yazarın İsmi : " + book.getBookOwner() +
-                    ",\t Yayın Tarihi : " + book.getPublishDate());
-        }
+        System.out.println("Sorted");
+        //Sorted (sıralama yapar) (reverseOrder() unutma lazım olcak)
+        list.stream().sorted(Comparator.reverseOrder()).forEach(i -> System.out.println(i));
 
-        //kitap sayfasına göre sıralama.
-        TreeSet<Book> books2 = new TreeSet<>(new Comparator<Book>() {
-            @Override
-            public int compare(Book o1, Book o2) {
-                return o1.getPageCount() - o2.getPageCount();
-            }
-        });
-        books2.add(book1);
-        books2.add(book2);
-        books2.add(book3);
-        books2.add(book4);
-        books2.add(book5);
-        System.out.println("Sayfa sayısına göre:");
-        for (Book book : books2) {
-            System.out.println("Kitap adı : " + book.getName() +
-                    ",\t Sayfa Sayısı : " + book.getPageCount() +
-                    ",\t Yazarın İsmi : " + book.getBookOwner() +
-                    ",\t Yayın Tarihi : " + book.getPublishDate());
-        }
+        System.out.println("Limit");
+        //Limit (Belirli sayıda eleman alma fonksiyonu)
+        list.stream().limit(5).forEach(i->System.out.println(i));
+
+        System.out.println("Skip");
+        //Skip (Belir sayıda elemanı atlar)
+        list.stream().skip(3).limit(5).distinct().sorted(Comparator.reverseOrder()).forEach(a -> System.out.println(a));
+
+        System.out.println("Count");
+        //Count (Eleman sayısını söyler)
+        long c = list.stream().skip(1).limit(5).count();
+        System.out.println(c);
+
+        System.out.println("AnyMatch");
+        //AnyMatch (Eşleşen koşul varsa ture yoksa false döndürür)
+        boolean control = list.stream().anyMatch(i -> i < 10);
+        System.out.println(control);
+
+        System.out.println("AllMatch");
+        //allMatch (Bütün koşular eşleşiyorsa ture yoksa false döndürür)
+        boolean control2 = list.stream().allMatch(i -> i  < 100);
+        System.out.println(control2);
+
+        System.out.println("NoneMatch");
+        //noneMatch (Eşleşmeyen koşul varsa ture yoksa false döndürür)
+        boolean control3 = list.stream().noneMatch(i -> i < 10);
+        System.out.println(control3);
+
+        System.out.println("Map");
+        // map (liste içindeki elemanlar üzerinde işlemler yapmamızı sağlıyor)
+        list.stream().map(i->i*2).forEach(i -> System.out.println(i));
+
+
     }
+
 }
